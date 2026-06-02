88ccb1ad9cde2569caf20ad3868706dd1467c9e7
riege <95230893+zolkers@users.noreply.github.com>
2026-06-02 19:27:55 +0200

    fix: clean sponge adapter boundary

--- modules/adapters/minecraft/sponge/src/main/java/dev/riege/buildmycommand/adapters/minecraft/sponge/SpongeCommandRegistrar.java
+++ modules/adapters/minecraft/sponge/src/main/java/dev/riege/buildmycommand/adapters/minecraft/sponge/SpongeCommandRegistrar.java
@@ -1,7 +1,5 @@
 package dev.riege.buildmycommand.adapters.minecraft.sponge;
 
-import java.util.Objects;
-
 public interface SpongeCommandRegistrar<C> {
     void register(Object pluginContainer, C command, String alias, String[] aliases);
 }
